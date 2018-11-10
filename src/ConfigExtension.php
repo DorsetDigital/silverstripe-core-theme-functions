006c5847bfd5b959a07508f9b56e101a14ad1ef3
Tim Burt <tim@dorset-digital.net>
2018-11-10 10:22:52 +0000

    Add config

--- src/ConfigExtension.php
+++ src/ConfigExtension.php
@@ -1,9 +1,27 @@
 <?php
+
 namespace DorsetDigital\Themes\Derriford;
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Assets\Image;
 
 class ConfigExtension extends DataExtension
 {
+    private static $db = [
+        'CopyrightText' => 'Varchar(200)',
+        'Telephone' => 'Varchar(50)',
+        'Email' => 'Varchar(255)'
+    ];
 
+    private static $many_many = [
+        'MainLogo' => Image::class,
+        'MobileLogo' => Image::class,
+        'FooterLogo' => Image::class
+    ];
+
+    private static $owns = [
+        'MainLogo',
+        'MobileLogo',
+        'FooterLogo'
+    ];
 }
